1056de3706a517623ed04d344edb79b0b0420b97
Ayush Singh <ayush.singh21082@gmail.com>
2019-07-22 17:27:27 +0530

    Add files via upload

---
+++ BasicCollectionView/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  BasicCollectionView
+//
+//  Created by EDUMVA4 on 21/07/19.
+//  Copyright © 2019 Kreative Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var countries = ["India","USA","Australia","Russia","China"]
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return countries.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+        
+        
+        
+        cell.myLabel.text = countries[indexPath.item]
+        cell.myLabel.textColor = .white
+        cell.backgroundColor = UIColor.red // make cell more visible in our example project
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+        let countrySelected = countries[indexPath.item]
+        print(countrySelected)
+        
+    }
+    
+
+
+}
+
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+}
+
+
